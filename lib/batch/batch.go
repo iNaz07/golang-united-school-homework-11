15e3c34dc7aeddd6ea2eb001906f362c0ff3e46a
00053269 <nazerkesa2@halykbank.kz>
2022-07-08 15:20:34 +0600

    init

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,10 @@
 package batch
 
 import (
+	"context"
+	"fmt"
+	"golang.org/x/sync/semaphore"
+	"sync"
 	"time"
 )
 
@@ -14,5 +18,38 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+
+	sem := semaphore.NewWeighted(pool)
+	ctx := context.Background()
+	mx := &sync.Mutex{}
+	for i := 0; i < int(n); i++ {
+		if err := sem.Acquire(ctx, 1); err != nil {
+			fmt.Println(fmt.Errorf("wait for resources: %w", err))
+		}
+		go func(i int64) {
+			u := getOne(i)
+			mx.Lock()
+			res = append(res, u)
+			mx.Unlock()
+			sem.Release(1)
+		}(int64(i))
+	}
+	if err := sem.Acquire(ctx, pool); err != nil {
+		fmt.Println(fmt.Errorf("wait for resources: %w", err))
+	}
+	//
+	//var wg sync.WaitGroup
+	//sema := make(chan struct{}, pool)
+	//
+	//for i := 0; i < int(n); i++ {
+	//	wg.Add(1)
+	//	sema <- struct{}{}
+	//	go func(i int64) {
+	//		res = append(res, getOne(i))
+	//		<-sema
+	//		wg.Done()
+	//	}(int64(i))
+	//}
+	//wg.Wait()
+	return
 }
